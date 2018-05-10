aec1cd6ab1702d9e544c768d763ed5c4ae81404b
Matthew Richardson <matthew.richardson@ncfunds.com>
2018-05-10 16:36:50 -0400

    dev

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { HashRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import Home from './pages/Home.jsx';
 import About from './pages/About.jsx';
