3040462fd6742a2cefa0890a4f674a2c9aaac970
Matthew Richardson <matthew.richardson@ncfunds.com>
2018-05-11 08:48:27 -0400

    switch added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import Home from './pages/Home.jsx';
 import About from './pages/About.jsx';
@@ -9,11 +9,11 @@ class App extends Component {
   render() {
     return (
       <Router basename={process.env.PUBLIC_URL}>
-        <div>
+        <Switch>
             <Route exact path="/" component={Home}/>
             <Route exact path="/about" component={About}/>
             <Route exact path="/contact" component={Contact}/>
-        </div>
+        </Switch>
       </Router>
     );
   }
