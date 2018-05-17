9bfd6bae2d28851bff4447f8f6fc4afbcfd91e0f
Matthew Richardson <mattlr20@gmail.com>
2018-05-17 13:35:08 -0400

    dev

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,14 +7,21 @@ export default class About extends Component {
     return (
       <div>
         <Grid>
-          <Col xs={12} sm={8} smOffset={2}>
-            <Image src="assets/koann-1.jpg" className="about-profile-pic" rounded />
+          <Col xs={12} sm={7}>
             <h3>About</h3>
             <p>Hello there. My name is Ko-Ann, and thanks for stopping by!</p>
-            <p>I live in Hollister, NC. I love to capture the emotions we all go through as people, particularly, those in love. My personal mission for each and every one of my clients is to create a time capsule of images for you and your loved ones to cherish for a lifetime.
-              I see beauty in all people and strive to make my clients feel comfortable in front of the camera in order to capture you at your best. Love is in the details. With every milestone in life, big or small, it deserves extra attention. I am committed to your special day because the only thing that matters is you. Trusting me to capture and share the making of memories is something I deeply appreciate.
-              I will provide an unforgettable experience by documenting your love with everything I have as if it were my own. After the moments have come and gone, I want my images to tell your love story and take your breath away over and over again.
-              Your love is one of a kind, and it would give me great pleasure to help you share it.</p>
+            <p>I live in Hollister, NC. I love to capture the emotions we all go through as people, particularly, those in love.</p>
+            <p>My personal mission for each and every one of my clients is to create a time capsule of images for you and your loved 
+              ones to cherish for a lifetime. I see beauty in all people and strive to make my clients feel comfortable in front of 
+              the camera in order to capture you at your best. Love is in the details. With every milestone in life, big or small, 
+              it deserves extra attention. I am committed to your special day because the only thing that matters is you. Trusting 
+              me to capture and share the making of memories is something I deeply appreciate.
+              I will provide an unforgettable experience by documenting your love with everything I have as if it were my own. After 
+              the moments have come and gone, I want my images to tell your love story and take your breath away over and over again.</p>
+            <p>Your love is one of a kind, and it would give me great pleasure to help you share it.</p>
+          </Col>
+          <Col xs={12} sm={5}>
+            <Image src="assets/koann-1.jpg" className="about-profile-pic" />
           </Col>
         </Grid>
       </div>
--- src/components/Portraits.jsx
+++ src/components/Portraits.jsx
@@ -1,18 +1,42 @@
 import React, { Component } from 'react'
-import { Grid, Col } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import { Grid, Image, Row, Col, Modal } from 'react-bootstrap';
 import './Portraits.css';
+
+const images = ['wedding-1','wedding-2','wedding-3','wedding-4',
+  'wedding-5','wedding-6','wedding-7','wedding-8','wedding-9','wedding-10','wedding-11','wedding-12']
+   const gallery = images.map( (img,index) => {
+    return (
+      <Col key={index} xs={12} sm={4} md={3} className="gal-item">
+        <div className="box">
+          <Link to="#" className="gal-link" data-toggle="modal" data-target={`#${index}`}>
+          <Image src={`assets/home/${img}.jpg`} />
+          </Link>
+          <div className="modal fade" id={index} tabIndex="-1" role="dialog">
+            <Modal.Dialog>
+                <Modal.Body>
+                  <button type="button" className="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button>
+                  <Image src={`assets/home/${img}.jpg`} />
+                  <div md={12} className="description">
+                    <h4>Wedding Photos</h4>
+                  </div>
+                </Modal.Body>
+            </Modal.Dialog>
+          </div>
+        </div>
+      </Col>
+    )
+   });
 
 export default class Portraits extends Component {
   render() {
     return (
-      <div>
-        <Grid>
-          <Col xs={12} sm={8} smOffset={2}>
-            <h3>Portraits</h3>
-            <p>Portraits</p>
-          </Col>
-        </Grid>
-      </div>
-    )
+      <Grid>
+        <Row>
+          <h3>Portrait Photos</h3>
+          {gallery}
+        </Row>
+      </Grid>
+    );
   }
 }
