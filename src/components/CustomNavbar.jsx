16338408e3e1fc6d4968e5045368a54c53a0ed5e
Matthew Richardson <mattlr20@gmail.com>
2018-05-18 13:34:42 -0400

    dev

--- src/components/CustomNavbar.jsx
+++ src/components/CustomNavbar.jsx
@@ -25,15 +25,19 @@ export default class CustomNavbar extends Component {
           <Nav>
             <NavItem eventKey={1} componentClass={Link} href="/" to="/">
               Home
+              {/* <div className="menu-description">Return Home</div> */}
             </NavItem>
             <NavItem eventKey={2} componentClass={Link} href="/about" to="/about">
               About KoAnn
+              {/* <div className="menu-description">Learn more</div> */}
             </NavItem>
             <NavItem eventKey={3} componentClass={Link} href="/weddings" to="/weddings">
               Weddings
+              {/* <div className="menu-description">Wedding Photo Gallery</div> */}
             </NavItem>
             <NavItem eventKey={4} componentClass={Link} href="/portraits" to="/portraits">
               Portraits
+              {/* <div className="menu-description">Portrait Photo Gallery</div> */}
             </NavItem>
           </Nav>
         </Navbar.Collapse>
