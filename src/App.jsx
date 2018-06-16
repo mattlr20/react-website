04692e0d83f25a7b4e04080d9a68d201b65fd78c
Matthew Richardson <mattlr20@gmail.com>
2018-06-16 13:52:47 -0400

    dev

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,26 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import 'babel-polyfill';
 
 import Home from './components/Home';
 import About from './components/About';
-import Contact from './components/Contact';
+// import Contact from './components/Contact';
 import Navbar from './components/CustomNavbar';
 import Footer from './components/footer';
 
 class App extends Component {
+  
+
   render() {
     return (
       <Router basename={process.env.PUBLIC_URL}>
         <div>
           <Navbar />
-          <Route exact path="/" component={Home} />
-          <Route exact path="/about" component={About} />
-          <Route exact path="/contact" component={Contact} />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/about" render={(props) => (<About />)} />
+            {/* <Route exact path="/contact" render={(props) => (<Contact />)} /> */}
+          </Switch>
           <Footer />
         </div>
       </Router>
