4438a5a529ff12eb230cc40963f56df053ba24eb
Matthew Richardson <matthew.richardson@ncfunds.com>
2018-06-19 16:42:56 -0400

    dev

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -5,7 +5,7 @@ export default class Footer extends Component {
     render() {
       return (
         <footer>
-            <p>KoAnns Photography &copy;</p>
+            <p>KoAnns Photography 2018 &copy;</p>
         </footer>
       )
     }
